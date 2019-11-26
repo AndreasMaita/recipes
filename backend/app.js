f3d76b76f9e269fcd41f85c6994c855dc1463445
AndreasMaita <A.maita@web.de>
2019-11-26 22:49:26 +0100

    added some backend funktionality. will try to connect my angular app with my backend

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/recipe-app'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/recipe-app/index.html'));
+app.use('/api/shoppingitems', (req, res, next) => {
+  const ingredients = [
+    {name: 'eggs'},
+    {name: 'milk'}
+  ];
+  res.json(ingredients);
 });
 
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/recipe-app'));
+
+// app.get('/*', function(req,res) {
+
+// res.sendFile(path.join(__dirname+'/dist/recipe-app/index.html'));
+// });
 
 
-
-module.export = app;
+module.exports = app;
