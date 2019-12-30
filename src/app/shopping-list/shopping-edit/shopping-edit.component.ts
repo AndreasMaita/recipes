42ed588d0d919353967f3fc00b842a402ea086ad
AndreasMaita <A.maita@web.de>
2019-12-30 20:59:08 +0100

    added Form reset

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ListsService } from '../list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -12,8 +13,13 @@ export class ShoppingEditComponent {
 
   constructor(public listService: ListsService) {}
 
-  onAddItem() {
-    this.listService.addList(this.enteredItemName, this.enteredAmount);
+  onAddItem(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.listService.addList(form.value.enteredItemName, form.value.enteredAmount);
+
+    form.reset();
   }
 
 }
